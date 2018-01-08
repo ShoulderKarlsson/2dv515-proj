294b7f6fb47708a46d611cf497ac7385ee671f55
shoulderkarlsson <shoulderkarlsson@gmail.com>
2018-01-08 21:26:00 +0100

    feat WIP: changed implementation of user ratings, favoring hashmap and started reimplement calculations of weightedscores

--- complete/src/main/java/main/lib/Recommender.java
+++ complete/src/main/java/main/lib/Recommender.java
@@ -1,6 +1,7 @@
 package main.lib;
 
 import org.springframework.util.ResourceUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -46,7 +47,8 @@ public class Recommender {
                             db.put("User" + userId, u);
                         }
 
-                        u.addRating(new Rating(this.movies.get(movieId), rating));
+//                        u.addRating(new Rating(this.movies.get(movieId), rating));
+                        u.add_rating(this.movies.get(movieId), rating);
                     });
         } catch (IOException e) {
             e.printStackTrace();
@@ -61,12 +63,82 @@ public class Recommender {
             if (db.get(k).getUsername().equals(user.getUsername())) continue;
 
             User otherUser = db.get(k);
-            res.add(new Result(otherUser, calc_Euclidian(otherUser, user)));
+            res.add(new Result(otherUser, _calc_Euclidian(otherUser, user)));
         }
 
-        // Each users weighted scores for each movie that they have rated.
-        HashMap<User, HashMap<String, Double>> userWeightedScores = getWeightedScores(res);
-        HashMap<String, Double> movieTotalWeightedScore = getMovieTotalScore(userWeightedScores);
+        /**
+         * This contains the wheightedscore for each of the users.
+         * The weighted score is calculated with the similarity * the rating a user has given for a specific movie.
+         *
+         * User - a user
+         * HashMap<String, Double>
+         *      String => Name of the movie
+         *      Double => Calculated score
+         */
+        HashMap<User, HashMap<String, Double>> weightedScoreForUsersPerMovie = new HashMap<>();
+        for (Result r : res) {
+            HashMap<String, Double> userRatings = r.user.get_ratings();
+            HashMap<String, Double> weightedScores = new HashMap<>();
+            userRatings
+                    .keySet()
+                    .stream()
+                    .forEach(movieName -> {
+                        double score = r.user.getRating(movieName);
+                        weightedScores.put(movieName, r.simScore * score);
+                    });
+
+            weightedScoreForUsersPerMovie.put(r.user, weightedScores);
+        }
+
+        /**
+         * This contains the total weightedscore for a single movie.
+         * This is done by adding together they wheighted scores for all movies and all of the scores.
+         * HashMap<String, Double>
+         *     String => Name of the movie
+         *     Double => Total weighted score for the movie
+         */
+        HashMap<String, Double> movieTotalWeightedScore = new HashMap<>();
+        weightedScoreForUsersPerMovie.keySet().stream()
+                .forEach(u -> {
+                    HashMap<String, Double> userWeightedScores = weightedScoreForUsersPerMovie.get(u);
+                    userWeightedScores.keySet().stream()
+                            .forEach(movieName -> {
+
+                                // If the set already contains a score for the movie
+                                // update it by increasing the total score
+                                if (movieTotalWeightedScore.containsKey(movieName)) {
+                                    double currentMovieScore = movieTotalWeightedScore.get(movieName);
+                                    currentMovieScore += userWeightedScores.get(movieName);
+                                    movieTotalWeightedScore.replace(movieName, currentMovieScore);
+                                } else {
+                                    movieTotalWeightedScore.put(movieName, userWeightedScores.get(movieName));
+                                }
+                            });
+                });
+
+
+        /**
+         * This contains the sum of all similarities for the users that has rated that movie.
+         * HashMap<String, Double>
+         *     String => Name of the movie
+         *     Double => Sum of the similarity
+         */
+        HashMap<String, Double> movieSumOfSimilarities = new HashMap<>();
+        movies.keySet().stream()
+                .forEach(movieId -> {
+                    String movieName = movies.get(movieId);
+                    weightedScoreForUsersPerMovie.keySet().stream()
+                            .forEach(u -> {
+                                // if the user has rated the current movie
+                                // add its similiarity score to the total for that movie
+                                if (u.get_ratings().containsKey(movieName)) {
+                                    if (movieSumOfSimilarities.containsKey(movieName)) {
+                                        
+                                    }
+                                }
+                            });
+                });
+
 
         /**
          * What works?
@@ -74,6 +146,11 @@ public class Recommender {
          * I can calculate each users specific weighted score for all movies rated
          * I can caluclate the sum of all similiarities for a movie
          */
+        // Each users weighted scores for each movie that they have rated.
+//        HashMap<User, HashMap<String, Double>> userWeightedScores = getWeightedScores(res);
+//        HashMap<String, Double> movieTotalWeightedScore = getMovieTotalScore(userWeightedScores);
+//        HashMap<String, Double> similaritySumForEachMovie = calculateSumSim(userWeightedScores);
+
 
 //        HashMap<String, User> n_recs = new HashMap<>();
 //        Iterator<User> it =  db.values().iterator();
@@ -107,75 +184,93 @@ public class Recommender {
         return null;
     }
 
-    private HashMap<String, Double> getMovieTotalScore(HashMap<User, HashMap<String, Double>> userWeightedScores) {
-        HashMap<String, Double> movieTotalScore = new HashMap<>();
-        for (User u : userWeightedScores.keySet()) {
-            HashMap<String, Double> userScores = userWeightedScores.get(u);
-            for (String movieName : userScores.keySet()) {
-                double movieScore = userScores.get(movieName);
-                if (movieTotalScore.containsKey(movieName)) {
-                    movieTotalScore.replace(movieName, movieTotalScore.get(movieName) + movieScore);
-                } else {
-                    movieTotalScore.put(movieName, movieScore);
-                }
-            }
-        }
+//    private HashMap<String, Double> getMovieTotalScore(HashMap<User, HashMap<String, Double>> userWeightedScores) {
+//        HashMap<String, Double> movieTotalScore = new HashMap<>();
+//        for (User u : userWeightedScores.keySet()) {
+//            HashMap<String, Double> userScores = userWeightedScores.get(u);
+//            for (String movieName : userScores.keySet()) {
+//                double movieScore = userScores.get(movieName);
+//                if (movieTotalScore.containsKey(movieName)) {
+//                    movieTotalScore.replace(movieName, movieTotalScore.get(movieName) + movieScore);
+//                } else {
+//                    movieTotalScore.put(movieName, movieScore);
+//                }
+//            }
+//        }
+//
+//        return movieTotalScore;
+//    }
+//
+//
+//    /**
+//     * Calculates each users weighted score for all the movies that the specific user has watched
+//     */
+//    HashMap<User, HashMap<String, Double>> getWeightedScores(ArrayList<Result> results) {
+//
+//        // Key - A user
+//        // Hashmap with weighted scores for each movie for the specific user
+//        HashMap<User, HashMap<String, Double>> weightedScores = new HashMap<>();
+//        for (Result r : results) {
+//            HashMap<String, Double> scores = new HashMap<>();
+//            for (Rating rating : r.user.ratings) {
+//                scores.put(rating.movie, r.simScore * rating.score);
+//            }
+//
+//            weightedScores.put(r.user, scores);
+//        }
+//
+//        return weightedScores;
+//    }
+//
+//    HashMap<String, Double> calculateSumSim(HashMap<User, HashMap<String, Double>> scores) {
+//        HashMap<String, Double> movieSumSimScores = new HashMap<>();
+//        for (int movieId : movies.keySet()) {
+//            String movieName = movies.get(movieId);
+//            double movieSumSimScore = 0.0;
+//            for (User u : scores.keySet()) {
+//                HashMap<String, Double> userScores = scores.get(u);
+//                if (userScores.containsKey(movieName)) {
+//                    movieSumSimScore += userScores.get(movieName);
+//                }
+//            }
+//            movieSumSimScores.put(movieName, movieSumSimScore);
+//        }
+//
+//
+//        return movieSumSimScores;
+//    }
 
-        return movieTotalScore;
-    }
 
+//    private double calc_Euclidian(User A, User B) {
+//        double sim = 0.0;
+//        int cnt_sim = 0;
+//
+//        for (Rating aRating : A.ratings) {
+//            for (Rating bRating : B.ratings) {
+//                // TODO: Redo matches? Not really sure the comparison is correct
+////                if (aRating.matches(bRating)) {
+//                    sim += Math.pow(aRating.score - bRating.score, 2);
+//                    cnt_sim++;
+////                }
+//            }
+//        }
+//
+//        return cnt_sim == 0 ? 0 : 1.0 / (1.0 + sim);
+//    }
 
-    /**
-     * Calculates each users weighted score for all the movies that the specific user has watched
-     */
-    HashMap<User, HashMap<String, Double>> getWeightedScores(ArrayList<Result> results) {
-
-        // Key - A user
-        // Hashmap with weighted scores for each movie for the specific user
-        HashMap<User, HashMap<String, Double>> weightedScores = new HashMap<>();
-        for (Result r : results) {
-            HashMap<String, Double> scores = new HashMap<>();
-            for (Rating rating : r.user.ratings) {
-                scores.put(rating.movie, r.simScore * rating.score);
-            }
-
-            weightedScores.put(r.user, scores);
-        }
-
-        return weightedScores;
-    }
-
-    void calculateSumSim(HashMap<User, HashMap<String, Double>> scores) {
-        HashMap<String, Double> movieSumSimScores = new HashMap<>();
-        for (int movieId : movies.keySet()) {
-            String movieName = movies.get(movieId);
-            double movieSumSimScore = 0.0;
-            for (User u : scores.keySet()) {
-                HashMap<String, Double> userScores = scores.get(u);
-                if (userScores.containsKey(movieName)) {
-                    movieSumSimScore += userScores.get(movieName);
-                }
-            }
-            movieSumSimScores.put(movieName, movieSumSimScore);
-        }
-
-        System.out.println(movieSumSimScores);
-
-
-    }
-
-
-    private double calc_Euclidian(User A, User B) {
+    private double _calc_Euclidian(User A, User B) {
         double sim = 0.0;
         int cnt_sim = 0;
+        HashMap<String, Double> ARatings = A.get_ratings();
+        HashMap<String, Double> BRatings = B.get_ratings();
 
-        for (Rating aRating : A.ratings) {
-            for (Rating bRating : B.ratings) {
-                // TODO: Redo matches? Not really sure the comparison is correct
-//                if (aRating.matches(bRating)) {
-                    sim += Math.pow(aRating.score - bRating.score, 2);
-                    cnt_sim++;
-//                }
+        for (String AKey : A.get_ratings().keySet()) {
+            for (String BKey : B.get_ratings().keySet()) {
+                // TODO: ===========================================
+                // TODO: There should be some sort of match here.. =
+                // TODO: ===========================================
+                sim += Math.pow(A.getRating(AKey) - B.getRating(BKey), 2);
+                cnt_sim++;
             }
         }
 
--- complete/src/main/java/main/lib/User.java
+++ complete/src/main/java/main/lib/User.java
@@ -2,36 +2,50 @@ package main.lib;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class User {
     private final String user;
-    public ArrayList<Rating> ratings = new ArrayList<>();
+//    public ArrayList<Rating> ratings = new ArrayList<>();
+    public HashMap<String, Double> _ratings = new HashMap<>();
 
     public User(String user) {
         this.user = user;
     }
 
-    public void addRating(Rating r) {
-        ratings.add(r);
+//    public void addRating(Rating r) {
+//        ratings.add(r);
+//    }
+
+    public void add_rating(String movie, double score) {
+        _ratings.put(movie, score);
     }
 
     /**
      * Change this to different datastructure in order to enhance performance?
      */
-    public Rating getRatingFor(String movie) {
-        Rating rating = null;
-
-        for (Rating r : ratings) {
-            if (r.movie.equals(movie)) {
-                rating = r;
-                break;
-            }
-        }
-
-        return rating;
-    }
+//    public Rating getRatingFor(String movie) {
+//        Rating rating = null;
+//
+//        for (Rating r : ratings) {
+//            if (r.movie.equals(movie)) {
+//                rating = r;
+//                break;
+//            }
+//        }
+//
+//        return rating;
+//    }
 
     public String getUsername() {
         return this.user;
+    }
+
+    public HashMap<String, Double> get_ratings() {
+        return _ratings;
+    }
+
+    public double getRating(String entryKey) {
+        return _ratings.get(entryKey);
     }
 }
