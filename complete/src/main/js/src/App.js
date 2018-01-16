ac5b783598aeccac67dc99859689af3c151dc73e
shoulderkarlsson <shoulderkarlsson@gmail.com>
2018-01-17 00:42:08 +0100

    feat: added routing

--- complete/src/main/js/src/App.js
+++ complete/src/main/js/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import * as React from 'react'
+import {Route} from 'react-router-dom'
+import {Users} from './pages/users'
+import {UserRecommendation} from './pages/user-recommendation'
 
 export const App = () => {
   return (
     <div>
-      <p>Hello World!</p>
+      <Route exact path="/" component={Users} />
+      <Route exact path="/user/:username" component={UserRecommendation} />
     </div>
   )
 }
--- complete/src/main/js/src/index.js
+++ complete/src/main/js/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+import {injectGlobal} from 'styled-components'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+injectGlobal`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+`
+ReactDOM.render(
+  <Router>
+    <Route path='/' component={App} />
+  </Router>,
+  document.getElementById('root'),
+)
