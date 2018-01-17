b6fe3b2c5c640404a942c40190163e48ad094f02
shoulderkarlsson <shoulderkarlsson@gmail.com>
2018-01-17 01:38:41 +0100

    feat: added spinkit

--- complete/src/main/js/src/components/with-fetch.js
+++ complete/src/main/js/src/components/with-fetch.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import {compose, lifecycle, withState} from 'recompose'
 import {get} from '../lib/http'
+import Spinner from 'react-spinkit'
+import {Fade} from '../components/fade'
 
 export const withFetch = ({
   url = '',
@@ -14,64 +16,38 @@ export const withFetch = ({
       async componentDidMount() {
         if (typeof urlGenerator === 'function' && url === '') {
           const url = urlGenerator(props)
-          // console.log('Url generated with generator: ', url)
-          // const response = await get(url).catch(e => console.log)
-          // console.log(response)
-          this.props.setData({
-            userRecs: {
-              User2: 0.10810810810810811,
-              User1: 0.2222222222222222,
-              User5: 0.11764705882352941,
-              User4: 0.3076923076923077,
-              User3: 0.23529411764705882,
-            },
-            movieRecs: {
-              Super: 3.8051914216761737,
-              Night: 3.3561594997806057,
-              Snake: 3.7146089293549807,
-              Luck: 2.461988486074374,
-              Dupree: 2.5725647213690217,
-              Lady: 2.852963389196576,
-            },
-          })
+          const response = await get(url).catch(e => console.log)
+          this.props.setData(response)
           return this.props.setIsLoading(false)
         }
 
-        // const response = await get(url)
-        //   .catch(e => this.props.setError(e))
+        const response = await get(url).catch(e => this.props.setError(e))
 
-        // setTimeout(() => {
-          const response = [
-            'User2',
-            'User1',
-            'User6',
-            'User5',
-            'User4',
-            'User3',
-          ]
+        setTimeout(() => {
           this.props.setData(response)
           this.props.setIsLoading(false)
-        // }, 1000)
+        }, 3000)
       },
     }),
   )
 
   const WithFetch = enhance(({isLoading, data, ...withFetchProps}) => {
-    console.log('isLoading :', isLoading)
     return isLoading ? (
-      <div
+      <Fade
         style={{
-          width: '100%',
-          height: '100%',
+          width: '100vw',
+          height: '100vh',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
         }}
       >
-        <p>Spinner..</p>
-      </div>
+        <Spinner fadeIn={'none'} name="pacman" />
+      </Fade>
     ) : (
-      <WrappedComponent data={data} {...withFetchProps} />
+      <Fade>
+        <WrappedComponent data={data} {...withFetchProps} />
+      </Fade>
     )
   })
 
